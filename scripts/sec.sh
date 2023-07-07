e9027792d1330e3a3fd03e7090c297a5e7eeeb99
JosemirAndrade <114767293+JosemirAndrade@users.noreply.github.com>
2023-07-07 16:18:55 -0300

    Add files via upload

---
+++ scripts/sec.sh
@@ -0,0 +1,6 @@
+#!/bin/bash#!/bin/bash
+
+
+zenity --info --width=250 --height=110 --text "<b><span color=\'green\'>CENTRAL DE PROGRAMAS</span></b> 
+A lista de programas será carregada em breve, porém antes de prosseguir, realize login como <b>administrador do sistema</b>"
+
